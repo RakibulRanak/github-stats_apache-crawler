29ceb42e8394bd27a0846f36d42916816ff4aa2d
MD RAKIBUL HASAN <43205114+RakibulRanak@users.noreply.github.com>
2025-05-17 23:27:55 +0600

    Implement programming language statistics of all repositories of a github user (#2)

---
+++ languages/README.md
@@ -0,0 +1,21 @@
+# Pre-requisites
+
+Install pip (if not already installed)
+
+Install the following libraries
+
+`pip install bs4 selenium requests pandas tqdm`
+
+Install cloc locally. This package is used to count lines of code in repositories. Find official instructions here : https://github.com/AlDanial/cloc#installing-cloc
+
+run `python3 fetch-language.py`
+
+provide the information while asked in prompt
+
+Finally, a json file will be created inside the current directoy with the statistics.
+
+Currently, Supported languages are :
+
+`["Python","JavaScript","Java","TypeScript","C#","C++","Go","PHP","Ruby","Bourne Shell"]` 
+
+This list can be modified in the code to add support for more languages
---
+++ languages/fetch-language.py
@@ -0,0 +1,108 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import pandas as pd
+import subprocess
+from tqdm import tqdm
+from selenium import webdriver
+import json
+import statistics
+
+USER = input("Enter github username: ")
+org_input = input("Is the user an organization? (yes / no): ")
+IS_ORG = org_input.lower() == 'yes'
+
+BASE_URL = 'https://github.com'
+
+def get_repo_names(page, user, is_org = False):
+    url = f'{BASE_URL}/orgs/{user}/repositories?page={page}' if is_org else f'{BASE_URL}/{user}?tab=repositories&page={page}'
+    repo_names = []
+
+    if is_org:
+        driver = webdriver.Chrome()
+        driver.get(url)
+        html = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html, 'html.parser')
+        repo_elements = soup.find_all('h4', class_=lambda c: c and 'Title-module__heading' in c) 
+        
+        for element in repo_elements:
+            name = element.find('a').text.strip()
+            repo_names.append(name)
+    else :
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        repo_elements = soup.find_all('h3', {'class': 'wb-break-all'})
+        for element in repo_elements:
+            name = element.find('a').text.strip()
+            repo_names.append(name)
+    return repo_names
+
+
+
+repo_urls = []
+repo_names = []
+page = 1
+while True:
+    names = get_repo_names(page, USER, IS_ORG)
+    if len(names) == 0:
+        break
+    repo_names.extend(names)
+    for name in names:
+        repo_url = f'{BASE_URL}/{USER}/{name}'
+        repo_urls.append(repo_url)
+    page +=1
+    
+print(repo_names)
+
+dir_path = f'clones/{USER}'
+os.makedirs(dir_path, exist_ok=True)
+
+df = pd.DataFrame({
+    'name' : repo_names,
+    'url' : repo_urls
+})
+df.to_csv(f'{dir_path}/repo_urls.csv', index=False)
+
+for url in tqdm(repo_urls):
+    subprocess.run(['git', 'clone', url], cwd=dir_path)
+
+languages = ["Python","JavaScript","Java","TypeScript","C#","C++","Go","PHP","Ruby","Bourne Shell"]
+
+path = f'clones/{USER}'
+lang_stats = {}
+
+for lang in languages:
+    lang_stats[lang] = []
+
+for repo in repo_names:
+    repo_path = os.path.join(path, repo)
+
+    result = subprocess.run(
+        ["cloc", repo_path, f"--include-lang={','.join(languages)}", "--json"],
+        capture_output=True,
+        text=True
+    )
+
+    data = json.loads(result.stdout)
+    repo_summary = {"repo_name": repo, "total": 0}
+
+    for lang in languages:
+        count = data.get(lang, {}).get("code", 0)
+        lang_stats[lang].append(count)
+
+total_summary = {}
+for lang in languages:
+    lang_counts = lang_stats[lang]
+    print(lang, lang_counts)
+    total = sum(lang_counts)
+    median = statistics.median(lang_counts) if lang_counts else 0
+    total_summary[lang] = {
+        "total" : total,
+        "median" : median
+    }
+
+with open(f'{USER}_language_summary.json', 'w') as f:
+    json.dump(total_summary, f)
+
+print(f'{USER}_language_summary.json created successfully' )
